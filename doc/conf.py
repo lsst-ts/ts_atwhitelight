415f9a643572507f31bbb9120f4afc76f62b696b
Colin Winslow <cwinslow@lsst.org>
2018-11-16 15:30:02 -0700

    initial commit

---
+++ doc/conf.py
@@ -0,0 +1,13 @@
+"""Sphinx configuration file for an LSST stack package.
+
+This configuration only affects single-package Sphinx documenation builds.
+"""
+
+from documenteer.sphinxconfig.stackconf import build_package_configs
+import lsst.ts.ATWhiteLightSource
+
+
+_g = globals()
+_g.update(build_package_configs(
+    project_name='ts_ATWhiteLightSource',
+    version=lsst.ts.ATWhiteLightSource.version.__version__))
