d47ddc1a0d76fee64934462fc5ca81f0340f185b
Colin Winslow <cwinslow@lsst.org>
2020-09-08 15:45:29 -0700

    changed main to amain for new salobj version

--- bin/wls_csc.py
+++ bin/wls_csc.py
@@ -5,9 +5,4 @@ import asyncio
 from lsst.ts.ATWhiteLightSource.wlsCSC import WhiteLightSourceCSC
 from lsst.ts.ATWhiteLightSource import version
 
-WhiteLightSourceCSC.main(index=False)
-#csc = WhiteLightSourceCSC()
-#print("launching white light CSC...")
-#dtask = csc.done_task
-#asyncio.get_event_loop().run_until_complete(csc.done_task)
-#print("Done")
+WhiteLightSourceCSC.amain(index=False)
