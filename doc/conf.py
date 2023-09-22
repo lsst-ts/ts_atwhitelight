560429aab33399e79392efc74e4fa1429879ee82
Eric Coughlin <ecoughlin@lsst.org>
2023-09-22 10:32:53 -0300

    Fix ruff ignore statements

--- doc/conf.py
+++ doc/conf.py
@@ -4,16 +4,16 @@ This configuration only affects single-package Sphinx documenation builds.
 """
 
 import lsst.ts.atwhitelight  # noqa: F401
-from documenteer.conf.pipelinespkg import *  # type: ignore # noqa: 403
+from documenteer.conf.pipelinespkg import *  # type: ignore # noqa: F403
 
 project = "ts_atwhitelight"
-html_theme_options["logotext"] = project  # type: ignore # noqa: 405
+html_theme_options["logotext"] = project  # type: ignore # noqa: F405
 html_title = project
 html_short_title = project
 # Avoid warning: Could not find tag file _doxygen/doxygen.tag
 doxylink = {}  # type: ignore
 
-intersphinx_mapping["ts_idl"] = ("https://ts-idl.lsst.io", None)  # type: ignore # noqa: 405
-intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # type: ignore # noqa: 405
-intersphinx_mapping["ts_tcpip"] = ("https://ts-tcpip.lsst.io", None)  # type: ignore # noqa: 405
-intersphinx_mapping["ts_utils"] = ("https://ts-utils.lsst.io", None)  # type: ignore # noqa: 405
+intersphinx_mapping["ts_idl"] = ("https://ts-idl.lsst.io", None)  # type: ignore # noqa: F405
+intersphinx_mapping["ts_salobj"] = ("https://ts-salobj.lsst.io", None)  # type: ignore # noqa: F405
+intersphinx_mapping["ts_tcpip"] = ("https://ts-tcpip.lsst.io", None)  # type: ignore # noqa: F405
+intersphinx_mapping["ts_utils"] = ("https://ts-utils.lsst.io", None)  # type: ignore # noqa: F405
